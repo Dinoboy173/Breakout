2668a99ed61ea20b8e033aefb2c89072c2338246
Jordan Wesson <s201031@students.aie.edu.au>
2021-02-10 12:25:00 +1100

    Player

---
+++ Breakout/Breakout/Player.cs
@@ -0,0 +1,41 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Breakout
+{
+    class Player
+    {
+        Program program;
+
+        public Vector2 pos = new Vector2();
+        public Vector2 size = new Vector2(250, 50);
+
+        public int speed = 5;
+
+        public Player(Program program, Vector2 pos, Vector2 size)
+        {
+            this.program = program;
+            this.pos = pos;
+            this.size = size;
+        }
+
+        public void Update()
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                pos.X = (pos.X - speed);
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+                pos.X = (pos.X + speed);
+
+            if (pos.X < (0 + (size.X / 2))) pos.X = (0 + (size.X / 2));
+            if (pos.X > (program.windowWidth - (size.X / 2))) pos.X = (program.windowWidth - (size.X / 2));
+        }
+
+        public void Draw()
+        {
+            Raylib.DrawRectanglePro(new Rectangle(pos.X, pos.Y, size.X, size.Y), size / 2, 0.0f, Color.BLUE);
+        }
+    }
+}
---
+++ Breakout/Breakout/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Raylib_cs;
+using System.Numerics;
+
+namespace Breakout
+{
+    class Program
+    {
+        public int windowWidth = 900;
+        public int windowHeight = 900;
+        public string windowTitle = "Breakout";
+
+        Player player;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.RunGame();
+        }
+
+        void RunGame()
+        {
+            Raylib.InitWindow(windowWidth, windowHeight, windowTitle);
+            Raylib.SetTargetFPS(60);
+
+            LoadGame();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Update();
+                Draw();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        void LoadGame()
+        {
+            player = new Player(
+                this,
+                new Vector2(windowWidth / 2, windowHeight),
+                new Vector2(250, 50));
+        }
+
+        void Update()
+        {
+            player.Update();
+        }
+
+        void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.DARKGRAY);
+
+            player.Draw();
+
+            Raylib.EndDrawing();
+        }
+    }
+}
